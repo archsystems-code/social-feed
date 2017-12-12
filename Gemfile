60083ea9135230b0db9a2f5c0585df7cab807c6d
Fprince333 <lwilsonsmith@gmail.com>
2017-12-12 16:23:12 -0500

    Add pry

--- Gemfile
+++ Gemfile
@@ -28,4 +28,5 @@ end
 group :test, :development do
   gem 'factory_girl'
   gem 'faker'
+  gem 'pry'
 end
--- config/environment.rb
+++ config/environment.rb
@@ -19,6 +19,7 @@ require 'twitter'
 
 require 'sinatra'
 require "sinatra/reloader" if development?
+require 'pry' if development?
 
 require 'erb'
 
