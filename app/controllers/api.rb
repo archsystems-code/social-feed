160f0a3c898be4e2e22fbf2f2185f5e1ebc201e5
Will Smith <lwilsonsmith@gmail.com>
2017-12-26 16:16:21 -0500

    Handle no such element

--- app/controllers/api.rb
+++ app/controllers/api.rb
@@ -11,9 +11,14 @@ get '/api/v1/feed' do
   posts = []
   driver.find_elements(class_name: "js-stream-item").each do |element|
     post = ""
-    post += element.find_element(class_name: "js-tweet-text-container").attribute("innerHTML") 
-    post += element.find_element(class_name: "AdaptiveMedia-photoContainer").attribute("innerHTML") 
-    posts.push(post)
+    begin
+      post += element.find_element(class_name: "js-tweet-text-container").attribute("innerHTML") 
+      post += element.find_element(class_name: "AdaptiveMedia-photoContainer").attribute("innerHTML") 
+    rescue Selenium::WebDriver::Error::NoSuchElementError
+      puts "No such element"
+    else
+      posts.push(post)
+    end
   end
   driver.get("https://www.instagram.com/explore/tags/asimaterials/")
   driver.find_elements(class_name: "_4rbun").each do |element|
