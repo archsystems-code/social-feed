905eb92a0867ff8a498d8a309025ccb42af5027c
Will Smith <lwilsonsmith@gmail.com>
2019-01-13 15:29:40 -0500

    Classname change

--- app/controllers/api.rb
+++ app/controllers/api.rb
@@ -12,8 +12,8 @@ get '/api/v1/feed' do
   driver.find_elements(class_name: "js-stream-item").each do |element|
     post = ""
     begin
-      post += element.find_element(class_name: "js-tweet-text-container").attribute("innerHTML") 
-      post += element.find_element(class_name: "AdaptiveMedia-photoContainer").attribute("innerHTML") 
+      post += element.find_element(class_name: "js-tweet-text-container").attribute("innerHTML")
+      post += element.find_element(class_name: "AdaptiveMedia-photoContainer").attribute("innerHTML")
     rescue Selenium::WebDriver::Error::NoSuchElementError
       puts "No such element"
     else
@@ -21,7 +21,7 @@ get '/api/v1/feed' do
     end
   end
   driver.get("https://www.instagram.com/explore/tags/asimaterials/")
-  driver.find_elements(class_name: "_4rbun").each do |element|
+  driver.find_elements(class_name: "eLAPa").each do |element|
     posts.push(element.attribute("innerHTML"))
   end
   driver.quit
